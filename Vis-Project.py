2afbb1fe7facc02b4c7a1c3800dfb49b65edca6c
Arad Peleg <163511154+aradpls@users.noreply.github.com>
2024-03-26 08:55:02 +0200

    Update Vis-Project.py

--- Vis-Project.py
+++ Vis-Project.py
@@ -49,7 +49,7 @@ for column in data4.columns[1:]:
         pass  # Handle non-numeric values here, for example setting them to NaN
 
 app = dash.Dash(__name__)
-app = app.sever
+app = app.server
 # Available years in the dataset for selection
 years = [str(year) for year in range(1948, 2022)]  # Adjust based on your dataset
 
